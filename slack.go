b38092578442b20e2c240ecfa0c4cd8f4858e216
Shinobu Okano Mac <okanoshinobu8@gmail.com>
2015-08-26 01:23:14 +0900

    Support format toml

--- slack.go
+++ slack.go
@@ -12,11 +12,11 @@ const (
 )
 
 type Slack struct {
-	Text       string `json:"text"`       //投稿内容
-	Username   string `json:"username"`   //投稿者名 or Bot名（存在しなくてOK）
-	Icon_emoji string `json:"icon_emoji"` //アイコン絵文字
-	IconURL    string `json:"icon_url"`   //アイコンURL（icon_emojiが存在する場合は、適応されない）
-	Channel    string `json:"channel"`    //#部屋名
+	Text      string `json:"text" toml:"text"`             //投稿内容
+	Username  string `json:"username" toml:"username"`     //投稿者名 or Bot名（存在しなくてOK）
+	IconEmoji string `json:"icon_emoji" toml:"icon_emoji"` //アイコン絵文字
+	IconURL   string `json:"icon_url" toml:"icon_url"`     //アイコンURL（icon_emojiが存在する場合は、適応されない）
+	Channel   string `json:"channel" toml:"channel"`       //#部屋名
 }
 
 type Payload struct {
