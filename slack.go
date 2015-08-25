419b64dc1d6fe40f33a3129c8178d8adc540a308
Shinobu Okano Mac <okanoshinobu8@gmail.com>
2015-08-25 21:57:48 +0900

    :sushi

--- slack.go
+++ slack.go
@@ -15,19 +15,19 @@ type Slack struct {
 	Text       string `json:"text"`       //投稿内容
 	Username   string `json:"username"`   //投稿者名 or Bot名（存在しなくてOK）
 	Icon_emoji string `json:"icon_emoji"` //アイコン絵文字
-	Icon_url   string `json:"icon_url"`   //アイコンURL（icon_emojiが存在する場合は、適応されない）
+	IconURL    string `json:"icon_url"`   //アイコンURL（icon_emojiが存在する場合は、適応されない）
 	Channel    string `json:"channel"`    //#部屋名
 }
 
 type Payload struct {
-	IncomingUrl string
+	IncomingURL string
 	Slack       Slack
 }
 
-func post(payload Payload) {
+func Post(payload Payload) {
 	params, _ := json.Marshal(payload.Slack)
 	resp, _ := http.PostForm(
-		payload.IncomingUrl,
+		payload.IncomingURL,
 		url.Values{ParameterPayload: {string(params)}},
 	)
 	body, _ := ioutil.ReadAll(resp.Body)
